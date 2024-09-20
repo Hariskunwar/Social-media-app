84d6999ef65b3a931b9183b7ee4b06802c569c03
Harish Kunwar <kunwarharish11@gmail.com>
2024-09-20 09:58:42 +0545

    Implement single post and user feed retrieval

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,11 +1,33 @@
 const Post=require('../models/postModel');
 const asyncErrorHandler=require("../utils/asyncErrorHandler");
-
+const CustomError = require('../utils/CustomError');
+const User=require("../models/userModel");
 
 exports.createPost=asyncErrorHandler(async (req,res,next)=>{
     req.body.postedBy=req.user._id;
     const post=await Post.create(req.body);
     res.status(201).json({
         data:post
+    });
+});
+
+
+//retrive single post
+exports.getPost=asyncErrorHandler(async (req,res,next)=>{
+    const post=await Post.findById(req.params.id);
+    if(!post){
+        return next(new CustomError('Post not found',404));
+    }
+    res.status(200).json({
+        data:post
+    });
+});
+
+//get posts of user followings
+exports.getFeed=asyncErrorHandler(async (req,res,next)=>{
+    const user=await User.findById(req.user._id);
+    const posts=await Post.find({postedBy:{$in:user.followings}});
+    res.status(200).json({
+        data:posts
     });
 });
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,9 +1,11 @@
 const express=require("express");
 const { protect } = require("../middlewares/authMiddleware");
-const { createPost } = require("../controllers/postController");
+const { createPost, getPost, getFeed } = require("../controllers/postController");
 
 const router=express.Router();
 
+router.get("/feed",protect,getFeed);
 router.post('/',protect,createPost);
+router.get("/:id",protect,getPost);
 
 module.exports=router;
